2dd67c71942534bfc4100c58b945373f6346f181
phuocding <phuocding@gmail.com>
2018-03-14 14:43:49 +0700

    non-bootstrap or any library/ traditional HTML and CSS

---
+++ HTML Template/objectJS.js
@@ -0,0 +1,16 @@
+var Comment = {
+    title : "",
+    content: "",
+    fullname: "",
+    email: "",
+    addComment: function(){
+        this.title = "Day la tieu de";
+    },
+    validateComment: function() {
+        this.content = "Day la noi dung";
+    }
+};
+
+Comment.addComment();
+Comment.title;
+
